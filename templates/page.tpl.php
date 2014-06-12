6235019176a3b88318cb7a73805d2d71360c0fcc
Stephen Truex <struex@ucar.edu>
2014-06-12 14:56:36 -0600

    More small changes to CSS etc

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -133,15 +133,16 @@
     <?php endif; ?>
     </div>
     
+    <?php if (!empty($site_slogan)): ?>
+      <p class="lead"><?php print $site_slogan; ?></p>
+    <?php endif; ?>
+        
   </div>
 </header>
 
 <div class="main-container container">
 
   <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
 
     <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
